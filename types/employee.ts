3ffdaa3dacafef915d1c2b6c6fca3fe7f7bb1702
eyualpha <eyuashenafi533@gmail.com>
2026-01-25 09:21:38 +0300

    feat: implement employee data fetching and display in EmployeesPage component

--- types/employee.ts
+++ types/employee.ts
@@ -10,6 +10,20 @@ export interface Employee {
   date_joined: string;
 }
 
+export interface User {
+  id?: number;
+  username?: string;
+  first_name?: string;
+  last_name?: string;
+  email?: string;
+  phone?: string;
+  position?: string;
+  department?: number;
+  is_active?: boolean;
+  date_joined?: string;
+}
+
+
 export interface CreateEmployee {
   first_name: string;
   last_name: string;
